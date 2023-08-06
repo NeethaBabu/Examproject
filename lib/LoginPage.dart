b31f986fd7c68a2f70be0c9eb826197094f51901
smash-cracker <dennythomas069@gmail.com>
2023-08-06 13:47:08 +0530

    login ui changes

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,16 +26,31 @@ class CardExamplesApp extends StatelessWidget {
             child: Stack(
               children: [
                 Positioned(
+                  top: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage("assets/images/circle.png"),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
                   bottom: 0,
                   left: 0,
                   right: 0,
                   top: 350,
                   child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                     elevation: 10,
                     color: Theme.of(context).colorScheme.surfaceVariant,
                     child: Padding(
                       padding: EdgeInsets.fromLTRB(40, 0, 20, 0),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           Row(
                             children: [
@@ -69,7 +86,24 @@ class CardExamplesApp extends StatelessWidget {
                           ElevatedButton(
                             child: Text('Continue'),
                             onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff342060),
+                                minimumSize: Size.fromHeight(50)),
                           ),
+                          RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                                text: 'By creating an account you agree to\n',
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                                  TextSpan(
+                                      text: 'our',
+                                      style: TextStyle(color: Colors.black)),
+                                  TextSpan(
+                                      text: 'Terms and condition',
+                                      style: TextStyle(color: Colors.blue)),
+                                ]),
+                          )
                         ],
                       ),
                     ),
